fdcf8af15d4c0f2b8a55b653ac4e1725d471ec4b
Ember Shan <ember@embers-mbp.wifi.local.cmu.edu>
2024-11-01 13:23:19 -0400

    small changes and cleaning up

--- src/Components/ImagePreview.js
+++ src/Components/ImagePreview.js
@@ -2,20 +2,27 @@ import { useState } from "react";
 
 function ImagePreview({ imgsrc, handleClose }) {
   return (
-    // <div>
-    //   <img
-    //     src={imgsrc}
-    //     alt="Uploaded Preview"
-    //     className="mt-4 w-48 h-48 object-cover rounded-md shadow-md"
-    //   />
-    // </div>
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
       <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+        {/* X button for closing */}
         <button
           onClick={handleClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
         >
-          X
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </button>
 
         {/* Image */}
